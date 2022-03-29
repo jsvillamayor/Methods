57547d27865688204b7d8f07b6d939abd646e927
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 16:59:20 -0700

    Add files via upload

---
+++ Methods/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "James";
+		hello(name);
+
+	}
+	
+	static void hello(String name) {
+		
+		System.out.println("Hello " + name + "!");
+		
+	}
+
+}
